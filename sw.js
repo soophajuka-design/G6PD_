714e32cd1181696674e10bd755a97437a38ec450
soophajuka-design <soophajuka@gmail.com>
2026-03-23 17:45:57 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,4 @@
-self.addEventListener('install', e => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => caches.delete(k)))
-    )
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
 });
