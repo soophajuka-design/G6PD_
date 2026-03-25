a692c346e23dca8e61b3e2e4440ec3d7d24c3870
soophajuka-design <soophajuka@gmail.com>
2026-03-25 18:29:16 +0700

    Create detect.js

---
+++ detect.js
@@ -0,0 +1,35 @@
+
+// detect.js
+
+function detectPaper(canvas) {
+  let src = cv.imread(canvas);
+  let gray = new cv.Mat();
+  cv.cvtColor(src, gray, cv.COLOR_RGBA2GRAY);
+
+  let blur = new cv.Mat();
+  cv.GaussianBlur(gray, blur, new cv.Size(5,5), 0);
+
+  let edges = new cv.Mat();
+  cv.Canny(blur, edges, 50, 150);
+
+  let contours = new cv.MatVector();
+  let hierarchy = new cv.Mat();
+
+  cv.findContours(edges, contours, hierarchy,
+    cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE);
+
+  let maxArea = 0;
+  let best = null;
+
+  for (let i = 0; i < contours.size(); i++) {
+    let cnt = contours.get(i);
+    let area = cv.contourArea(cnt);
+
+    if (area > maxArea) {
+      maxArea = area;
+      best = cnt;
+    }
+  }
+
+  return best; // contour ของกระดาษ
+}
