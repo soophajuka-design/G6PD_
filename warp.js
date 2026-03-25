b37e6dd40e2436f00fac3c9ae3045c66b0aa6368
soophajuka-design <soophajuka@gmail.com>
2026-03-25 19:56:31 +0700

    Update warp.js

--- warp.js
+++ warp.js
@@ -1,28 +1,8 @@
 
 // warp.js
 
-function warpPaper(src, contour) {
-  let rect = cv.boundingRect(contour);
+function warpPaper(canvas, contour) {
+  return cv.imread(canvas); // (versionแรก ใช้ตรงๆก่อน)
+}
 
-  let dst = new cv.Mat();
-  let dsize = new cv.Size(710, 1280); // scale 10x ของ 7.1x12.8
-
-  let srcTri = cv.matFromArray(4,1,cv.CV_32FC2, [
-    rect.x, rect.y,
-    rect.x + rect.width, rect.y,
-    rect.x + rect.width, rect.y + rect.height,
-    rect.x, rect.y + rect.height
-  ]);
-
-  let dstTri = cv.matFromArray(4,1,cv.CV_32FC2, [
-    0,0,
-    dsize.width,0,
-    dsize.width,dsize.height,
-    0,dsize.height
-  ]);
-
-  let M = cv.getPerspectiveTransform(srcTri, dstTri);
-  cv.warpPerspective(src, dst, M, dsize);
-
-  return dst;
 }
