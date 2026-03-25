c27ec95f02b1fe50565ba1c2b133975f06a7a2b1
soophajuka-design <soophajuka@gmail.com>
2026-03-25 20:00:57 +0700

    Update ai.js

--- ai.js
+++ ai.js
@@ -1,18 +1,10 @@
 // ai.js
 
-let model;
 
-async function loadModel() {
-  model = await tf.loadLayersModel('model/model.json');
-}
-
-async function predict(imageTensor) {
-  let pred = model.predict(imageTensor);
-  let data = await pred.data();
-
+async function predict() {
   return {
-    normal: data[0],
-    intermediate: data[1],
-    deficient: data[2]
+    normal: Math.random(),
+    intermediate: Math.random(),
+    deficient: Math.random()
   };
 }
