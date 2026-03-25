8adef12e4b30aa59b793d49292b4ba0693a730bb
soophajuka-design <soophajuka@gmail.com>
2026-03-25 20:01:55 +0700

    Update analyzer.js

--- analyzer.js
+++ analyzer.js
@@ -3,37 +3,17 @@
 // analyzer.js
 
 async function analyze() {
-  let canvas = captureFrame();
+  const frame = captureFrame();
 
-  let contour = detectPaper(canvas);
-  let warped = warpPaper(canvas, contour);
-
-  let results = [];
-
-  for (let r = 0; r < 5; r++) {
-    for (let c = 0; c < 4; c++) {
+  for(let r=0;r<5;r++){
+    for(let c=0;c<4;c++){
 
       let cell = gridState[r][c];
-      if (!cell.selected) continue;
+      if(!cell.selected) continue;
 
-      let roi = extractCell(warped, r, c);
+      let pred = await predict();
 
-      let tensor = tf.browser.fromPixels(roi)
-        .resizeNearestNeighbor([64,64])
-        .toFloat()
-        .div(255.0)
-        .expandDims();
-
-      let pred = await predict(tensor);
-
-      results.push({
-        row: r,
-        col: c,
-        prediction: pred,
-        type: cell.type
-      });
+      console.log(r,c,pred,cell.type);
     }
   }
-
-  console.log(results);
 }
