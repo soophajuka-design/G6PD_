6362da1818f6594e4d911b635f3f381433b3af37
soophajuka-design <soophajuka@gmail.com>
2026-03-27 14:54:56 +0700

    Update camera.js

--- camera.js
+++ camera.js
@@ -7,24 +7,14 @@ function initCameraElement(){
 async function startCamera(){
 
   const stream = await navigator.mediaDevices.getUserMedia({
-    video:true,
+    video:{
+      facingMode:{ ideal:"environment" } // ✅ กล้องหลัง
+    },
     audio:false
   });
 
   video.srcObject = stream;
   await video.play();
 
-  console.log("Camera started");
-}
-
-function captureFrame(){
-
-  const canvas = document.createElement('canvas');
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-
-  const ctx = canvas.getContext('2d');
-  ctx.drawImage(video,0,0);
-
-  return cv.imread(canvas);
+  console.log("Camera started (rear)");
 }
