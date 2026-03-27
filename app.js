091156e735b361218c535547275d6b406555907f
soophajuka-design <soophajuka@gmail.com>
2026-03-27 19:59:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,32 +6,9 @@ window.onload = () => {
 
   initCameraElement();
 
-  overlay = document.getElementById("overlay");
-  ctx = overlay.getContext("2d");
-
-  // ===== START BUTTON =====
-
-  document.getElementById('startBtn').onclick = () => {
-  startCameraDirect();
-};
-  // ===== TAP =====
-  overlay.addEventListener("click", (e)=>{
-
-    if(!isCameraOn || !geo) return;
-
-    const rect = overlay.getBoundingClientRect();
-
-    const scaleX = overlay.width / rect.width;
-    const scaleY = overlay.height / rect.height;
-
-    const x = (e.clientX - rect.left) * scaleX;
-    const y = (e.clientY - rect.top) * scaleY;
-
-    handleTap(x, y, geo);
-
-    // 🔥 redraw ทันที
-    drawNow();
-  });
+  document.getElementById('startBtn').onclick = async () => {
+    await startCamera();
+  };
 
 };
 
